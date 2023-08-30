009bc3592f13663519cb0cdd8d8e61885f6a09db
ihsahm <ihsahm11@gmail.com>
2023-08-30 20:39:48 +0300

    Second Commit

--- lib/bloc/auth_list_event.dart
+++ lib/bloc/auth_list_event.dart
@@ -1,1 +1,15 @@
-part of 'auth_list_bloc.dart';
+// part of 'auth_list_bloc.dart';
+
+
+
+// @immutable
+// abstract class AuthListEvent{
+
+// }
+
+
+// class RegisterUser extends AuthListEvent{
+//   final User user;
+
+//   RegisterUser({required this.user});
+// }
