fc6c9acd18762f7b698d36261c43d66b4cac3f71
ihsahm <ihsahm11@gmail.com>
2023-08-31 16:44:51 +0300

    Second Commit

--- lib/views/homepage.dart
+++ lib/views/homepage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:fluttericon/entypo_icons.dart';
+import 'package:fluttericon/linecons_icons.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,6 +10,13 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int _passenger = 0;
+  void incrementPassenger() {
+    setState(() {
+      _passenger++;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,18 +24,17 @@ class _HomePageState extends State<HomePage> {
         children: [
           topStack(),
           bottomStack(),
+          bottomButton(),
         ],
       ),
     );
   }
 
   Widget topStack() {
-    bool isReturnActive = true;
-    bool isOneWayActive = false;
     return Stack(
       children: [
-        Container(
-          height: 430,
+        SizedBox(
+          height: 400,
           width: double.infinity,
           child: Image.asset(
             "images/guzo.jpg",
@@ -83,7 +91,7 @@ class _HomePageState extends State<HomePage> {
             top: 230,
             child: Padding(
               padding: const EdgeInsets.only(left: 18.0),
-              child: Container(
+              child: SizedBox(
                 height: 200,
                 child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -158,10 +166,12 @@ class _HomePageState extends State<HomePage> {
 
   Widget bottomStack() {
     return Padding(
-      padding: const EdgeInsets.only(top: 8.0, left: 8.0),
+      padding: const EdgeInsets.only(top: 8.0, left: 8.0, right: 16.0),
       child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
           Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               const Text(
                 "Departure Date",
@@ -207,7 +217,173 @@ class _HomePageState extends State<HomePage> {
               ),
             ],
           ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                "Return Date",
+                style: TextStyle(color: Colors.grey, fontSize: 18),
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "10",
+                    style: TextStyle(color: Colors.blue[700], fontSize: 35),
+                  ),
+                  const SizedBox(
+                    width: 20,
+                  ),
+                  const Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "May",
+                        style: TextStyle(color: Colors.black, fontSize: 18),
+                      ),
+                      Text(
+                        "Wednesday",
+                        style: TextStyle(color: Colors.black, fontSize: 18),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              const Text(
+                "Passengers",
+                style: TextStyle(color: Colors.grey, fontSize: 18),
+              ),
+              const SizedBox(
+                height: 10.0,
+              ),
+              InkWell(
+                onTap: () {
+                  showModalBottomSheet(
+                      context: context,
+                      builder: (context) {
+                        return Container(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  const Text("Passengers",
+                                      style: TextStyle(
+                                          color: Colors.black, fontSize: 18)),
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.end,
+                                    children: [
+                                      TextButton(
+                                        onPressed: () {
+                                          Navigator.pop(context);
+                                        },
+                                        child: const Text("Cancel",
+                                            style:
+                                                TextStyle(color: Colors.black)),
+                                      ),
+                                      TextButton(
+                                        onPressed: () {
+                                          Navigator.pop(context);
+                                        },
+                                        child: const Text(
+                                          "Done",
+                                          style: TextStyle(color: Colors.black),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  const Text("Adult"),
+                                  Row(
+                                    children: [
+                                      IconButton(
+                                        icon: const Icon(Entypo.minus_circled),
+                                        onPressed: () {},
+                                      ),
+                                      const SizedBox(
+                                        width: 10,
+                                      ),
+                                      Text('$_passenger'),
+                                      const SizedBox(
+                                        width: 10,
+                                      ),
+                                      IconButton(
+                                        icon: const Icon(Entypo.plus_circled),
+                                        onPressed: incrementPassenger,
+                                      )
+                                    ],
+                                  ),
+                                ],
+                              )
+                            ],
+                          ),
+                        );
+                      });
+                },
+                child: const Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Icon(Icons.person),
+                    SizedBox(
+                      width: 3,
+                    ),
+                    Text("1"),
+                    SizedBox(
+                      width: 18,
+                    ),
+                    Icon(Icons.person_outline),
+                    SizedBox(
+                      width: 3,
+                    ),
+                    Text("0"),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Icon(Icons.child_care),
+                    SizedBox(
+                      width: 3,
+                    ),
+                    Text("0")
+                  ],
+                ),
+              ),
+            ],
+          ),
         ],
+      ),
+    );
+  }
+
+  Widget bottomButton() {
+    return Align(
+      alignment: Alignment.bottomCenter,
+      child: Padding(
+        padding: const EdgeInsets.only(top: 100.0, left: 40.0, right: 40.0),
+        child: SizedBox(
+          height: 50,
+          width: double.infinity,
+          child: ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.amber,
+              elevation: 0,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15)),
+            ),
+            child: const Text(
+              "Search Flights",
+              style: TextStyle(color: Colors.black),
+            ),
+            onPressed: () {},
+          ),
+        ),
       ),
     );
   }
